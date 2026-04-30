9e42c3642b6602a04411c99f6ba40d590ab939d5
dsol7191 <danielsolis@dsolcoaching.com>
2026-04-30 07:59:22 -0500

    syntax correction

--- src/main/java/nattypro/life/forum/ThreadFollowRepository.java
+++ src/main/java/nattypro/life/forum/ThreadFollowRepository.java
@@ -1,18 +1,21 @@
 package nattypro.life.forum;
+
 import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.transaction.annotation.Transactional;
 
 
-@Transactional
-void deleteByPost(Post post);
 
 public interface ThreadFollowRepository extends JpaRepository<ThreadFollow, Long> {
     Optional<ThreadFollow> findByUserAndPost(User user, Post post);
     List<ThreadFollow> findByUser(User user);
     boolean existsByUserAndPost(User user, Post post);
-    void deleteByPost(Post post);
+    
+
+@Transactional
+void deleteByPost(Post post);
+
 }
 
 
